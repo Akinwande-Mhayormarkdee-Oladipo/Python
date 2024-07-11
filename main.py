702cc20181dec944556f16e6c20da1859f3e73f3
Akinwande Oladipo <54485470+Akinwande-Mhayormarkdee-Oladipo@users.noreply.github.com>
2024-07-11 23:36:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,8 +35,35 @@ def money_process():
     total_amount = num_quarters*0.25 + num_dimes*0.1 + num_nickels*0.05 + num_pennies*0.01
     return total_amount
 
-def money_check(
-        
-)
 
-report(resources, amount=50)
+def money_check(amount, required_store):
+    if amount < required_store['cost']:
+        print('Sorry that is not enough money. Money refunded.')
+    else:
+        change = amount-required_store['cost']
+        print(f"Here is ${0:.2f} in change.".format(change))
+        return amount
+
+
+def coffee_maker():
+    machine_switch = True
+    available_items = resources
+    machine_amount = 0
+    while machine_switch:
+
+        coffee_type = drink_choice()
+        while coffee_type != 'espresso' and coffee_type != 'latte' and coffee_type != 'cappuccino':
+            print(f"You've chosen {coffee_type}")
+            if coffee_type == 'off':
+                print("Bye!")
+                machine_switch = False
+                break
+            coffee_store = MENU["coffee_type"]
+            if coffee_type == 'report':
+                report(available_items, machine_amount)
+
+
+
+
+
+
