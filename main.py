4c2ce5d9abfdcb28071978f2e204f0338ccc718d
Akinwande Oladipo <54485470+Akinwande-Mhayormarkdee-Oladipo@users.noreply.github.com>
2024-07-11 22:46:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,19 @@ from coffee_resources import MENU, resources
 
 
 def drink_choice():
-    print()
+    print("Drinks available: Expresso, Latte, Cappuccino.")
+    choice = input("Select a drink: ").lower()
+    return choice
+
+
+def compare_resource(store_qty, required_qty):
+    should_continue = True
+    for item in store_qty:
+        if store_qty[item] < required_qty[item]:
+            print(f"Sorry there is not enough {item}")
+            should_continue = False
+            return should_continue
+    return should_continue
 
 
 def report(store, amount):
@@ -14,5 +26,17 @@ def report(store, amount):
     print(f"Coffee: {store['coffee']}g")
     print(f"Money: ${amount}")
 
+
+def money_process():
+    num_quarters = int(input("Enter number of quarters: "))
+    num_dimes = int(input("Enter number of dimes: "))
+    num_nickels = int(input("Enter number of nickels: "))
+    num_pennies = int(input("Enter number of pennies: "))
+    total_amount = num_quarters*0.25 + num_dimes*0.1 + num_nickels*0.05 + num_pennies*0.01
+    return total_amount
+
+def money_check(
+        
+)
 
 report(resources, amount=50)
